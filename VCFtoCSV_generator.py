e4baada4b8b0578294c5ae04ef6d3ebcf6d74499
Hinrich <mohamadh.rezaie@gmail.com>
2020-07-10 19:12:46 -0700

    Snps collector script debugged

--- VCFtoCSV_generator.py
+++ VCFtoCSV_generator.py
@@ -3,28 +3,29 @@ import csv
 import os
 
 
-
 def genome_id_creator(path):
     gene_list = path
     genome_id = []
     with open(gene_list) as csvfile:
-            readCSV = csv.reader(csvfile, delimiter=',')
-            for row in readCSV:
-                genome_id.append(row)
+        readCSV = csv.reader(csvfile, delimiter=',')
+        for row in readCSV:
+            genome_id.append(row)
     return genome_id
 
 
 ### for this function, all non-folder files should be moved from the directory
-def snp_collector(input, output):
+def snp_collector(input,output,gene_name_path):
     output_path = output
-    files = os.listdir()
-    genome_id_creator()
+    files = os.listdir(input)
+    genome_id = genome_id_creator(gene_name_path)
     for f in files:
-        path = f + "/snps.csv"
-        order = "cp " + path + " ../SNPs/"
-        os.system()
-
+        if f != ".DS_Store":
+            path = input + f + "/snps.csv"
+            target_csv_name = str(genome_id[int(f)+1][1]) + ".csv"
+            order = "cp " + path + " " + output_path + target_csv_name
+            os.system(order)
+            
 
 if __name__ == "__main__":
-    genome_id = genome_id_creator("Escherichia_just_gene_numbers_unique.csv")
-    print(genome_id[1][1])
+    snp_collector("../files/","../SNPs/", "Escherichia_just_gene_numbers_unique.csv")
+    print("Done!")
