1e5e12700914b077ab2884e767d4e8c6fecdb450
Hinrich <mohamadh.rezaie@gmail.com>
2020-07-10 18:04:00 -0700

    snp colector function

--- VCFtoCSV_generator.py
+++ VCFtoCSV_generator.py
@@ -1,3 +1,25 @@
 import pandas
+import csv
+import os
 
-from SNP import p
+
+
+def genome_id_creator(path):
+    gene_list = path
+    genome_id = []
+    with open(gene_list) as csvfile:
+            readCSV = csv.reader(csvfile, delimiter=',')
+            for row in readCSV:
+                genome_id.append(row)
+    return genome_id
+
+
+### for this function, all non-folder files should be moved from the directory
+def snp_collector(input, output):
+    input_file = output
+    output_path = output
+    files = os.listdir()
+    for f in files:
+        path = f + "snps.csv"
+        order = "cp " + f + path
+        os.system()
